8d56b7500b65ebdbe6919cea802e5234bcdcb148
slimmekeuzesinhuis <etienne.tauwnaar@gmail.com>
2025-09-20 14:47:51 +0200

    Init Next.js skeleton (#6)
    * Create package.json
    
    centraal configuratiebestand voor Node/Next.js
    
    * Create next.config.js
    
    Next.js instellingen
    
    * Create layout.js
    
    basislayout voor alle pagina’s, app router
    
    * Create page.js
    
    startpagina

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export const metadata = { title: "Slimme keuzes in huis" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="nl">
+      <body>{children}</body>
+    </html>
+  );
+}
