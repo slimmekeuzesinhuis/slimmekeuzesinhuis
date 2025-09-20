a8783c325e651eb0adc0c900dbf2ce389c700cac
slimmekeuzesinhuis <etienne.tauwnaar@gmail.com>
2025-09-20 14:44:51 +0200

    Create page.js
    startpagina

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Slimme keuzes in huis</h1>
+      <p>Minimale Next.js startpagina ✅</p>
+    </main>
+  );
+}
